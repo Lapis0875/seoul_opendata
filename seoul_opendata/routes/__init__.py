09e9adccbaad8dbd8a44157d7fe5dc1cca4c214c
Lapis0875 <lapis0875@kakao.com>
2023-05-31 14:44:11 +0900

    fix: Add routers

--- seoul_opendata/routes/__init__.py
+++ seoul_opendata/routes/__init__.py
@@ -1,7 +1,11 @@
 from .user_route import user_router
 from .child_school_route import child_school_router
+from .child_route import child_router
+from .article_route import article_router
 
 __all__ = (
     "user_router",
-    "child_school_router"
+    "child_school_router",
+    "child_router",
+    "article_router"
 )
